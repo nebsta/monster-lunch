6d2cd4669de58ea46b16867de76d2ca213614a34
Benjamin Wallis <bewa@trustpilot.com>
2024-03-02 16:36:27 +0100

    fixing up formatting

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,26 @@
-#include <iostream>
-#include <core/Game.hpp>
 #include <SDL2/SDL.h>
+#include <core/Game.hpp>
+#include <iostream>
 
 int main() {
   std::cout << "Hello World";
 
-  if(SDL_Init(SDL_INIT_VIDEO) < 0)
-  {
+  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
     std::cout << "Failed to initialize the SDL2 library\n";
     return -1;
   }
 
-  SDL_Window *window = SDL_CreateWindow("SDL2 Window",
-      SDL_WINDOWPOS_CENTERED,
-      SDL_WINDOWPOS_CENTERED,
-      680, 480,
-      0);
+  SDL_Window *window = SDL_CreateWindow("SDL2 Window", SDL_WINDOWPOS_CENTERED,
+                                        SDL_WINDOWPOS_CENTERED, 680, 480, 0);
 
-  if(!window)
-  {
+  if (!window) {
     std::cout << "Failed to create window\n";
     return -1;
   }
 
   SDL_Surface *window_surface = SDL_GetWindowSurface(window);
 
-  if(!window_surface)
-  {
+  if (!window_surface) {
     std::cout << "Failed to get the surface from the window\n";
     return -1;
   }
@@ -35,15 +29,15 @@ int main() {
 
   SDL_Event e;
   bool quit = false;
-  while (!quit){
-    while (SDL_PollEvent(&e)){
-      if (e.type == SDL_QUIT){
+  while (!quit) {
+    while (SDL_PollEvent(&e)) {
+      if (e.type == SDL_QUIT) {
         quit = true;
       }
-      if (e.type == SDL_KEYDOWN){
+      if (e.type == SDL_KEYDOWN) {
         quit = true;
       }
-      if (e.type == SDL_MOUSEBUTTONDOWN){
+      if (e.type == SDL_MOUSEBUTTONDOWN) {
         quit = true;
       }
     }
