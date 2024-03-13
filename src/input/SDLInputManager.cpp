6cd5ecb6f005615d92fb2df1db4315230a5a58fc
Benjamin Wallis <bewa@trustpilot.com>
2024-03-13 15:04:15 +0100

    fixing window cleaing

--- src/input/SDLInputManager.cpp
+++ src/input/SDLInputManager.cpp
@@ -1,4 +1,5 @@
 #include "SDLInputManager.hpp"
+#include <SDL_events.h>
 #include <grumble/core/Object.hpp>
 
 SDLInputManager::SDLInputManager() : grumble::InputManager() {}
@@ -9,7 +10,10 @@ bool SDLInputManager::update() {
   SDL_Event e;
   bool terminate = false;
   while (SDL_PollEvent(&e)) {
-    if (e.type == SDL_KEYDOWN) {
+    if (e.type == SDL_QUIT) {
+      terminate = true;
+      break;
+    } else if (e.type == SDL_KEYDOWN) {
       auto name = SDL_GetKeyName(e.key.keysym.sym);
       auto inputCode = SDL2Utils::SDL2_Keycode_to_InputCode(e.key.keysym.sym);
       activateInput(inputCode);
