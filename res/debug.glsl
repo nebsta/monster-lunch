3439ebd6fbed0a3aaefe4706ef86c3a625be824d
Benjamin Wallis <bewa@trustpilot.com>
2024-03-15 18:49:50 +0100

    cleaning up more of the shader code

--- res/debug.glsl
+++ res/debug.glsl
@@ -18,19 +18,19 @@ out vec4 color;
 @include_block funcs
 
 void main() {
-  vec3 offset_pos = vec3(pos.x, pos.y + inst_off, pos.z);
+  vec2 offset_pos = vec2(pos.x, pos.y + inst_off);
   if (inst_ori == 1) { // vertical
-    offset_pos = vec3(offset_pos.y, offset_pos.x, pos.z);
+    offset_pos = vec2(offset_pos.y, offset_pos.x);
   }
-  offset_pos = vec3(offset_pos.x + off.x, offset_pos.y + off.y, offset_pos.z);
+  offset_pos = vec2(offset_pos.x + off.x, offset_pos.y + off.y);
 
   if (inst_ori == 0) { // horizontal
-    offset_pos = vec3(pos.x, sawtooth_wave(offset_pos.y), offset_pos.z);
+    offset_pos = vec2(pos.x, sawtooth_wave(offset_pos.y));
   } else { // vertical
-    offset_pos = vec3(sawtooth_wave(offset_pos.x), pos.x, offset_pos.z);
+    offset_pos = vec2(sawtooth_wave(offset_pos.x), pos.x);
   }
 
-  gl_Position = vec4(offset_pos, 1.0);
+  gl_Position = vec4(offset_pos, pos.z, 1.0);
   color = vec4(1.0f, 0.0f, 1.0f, 1.0f);
 }
 @end
